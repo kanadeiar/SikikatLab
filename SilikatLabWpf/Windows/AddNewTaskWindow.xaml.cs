0ee2eba4cd47e773d09ce7ae4bf6ccd6747fab33
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-03-03 14:40:14 +0400

    Окно добавления нового задания

---
+++ SilikatLabWpf/Windows/AddNewTaskWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace SilikatLabWpf.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewTaskWindow.xaml
+    /// </summary>
+    public partial class AddNewTaskWindow : Window
+    {
+        public AddNewTaskWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
